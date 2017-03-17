ca973e8325163b659e8a6ecab5849e405f70b578
afeiship <1290657123@qq.com>
2017-03-17 22:51:01 +0800

    update apache

--- scripts/initial_apache.sh
+++ scripts/initial_apache.sh
@@ -4,4 +4,5 @@ sudo rm -rf /etc/httpd/conf.d;
 sudo rm -rf /etc/httpd/conf;
 sudo ln -s /vagrant/centos7.2/conf.d /etc/httpd/conf.d;
 sudo ln -s /vagrant/centos7.2/conf /etc/httpd/conf;
+
 apache-restart;
