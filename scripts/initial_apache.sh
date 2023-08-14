93fad5a687e34a8c19260825c37553ded2a68145
afeiship <1290657123@qq.com>
2023-08-14 11:16:55 +0800

    feat: init file-cb plugin

--- scripts/initial_apache.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rm -rf /etc/httpd/conf.d;
-sudo rm -rf /etc/httpd/conf;
-sudo ln -s /vagrant/centos7.2/conf.d /etc/httpd/conf.d;
-sudo ln -s /vagrant/centos7.2/conf /etc/httpd/conf;
-
-apache-restart;
--- scripts/initial_local_ssh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# 输入密码登录后，到.ssh目录下，修改authorized_keys的权限为644，就可以不用输入密码登录了。
-cd ~/.ssh
-chmod 644 -R authorized_keys
