1ccb955b1fe8855d35991503501031b80ac66138
afeiship <1290657123@qq.com>
2023-08-14 11:21:15 +0800

    feat: add jon5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
+import fs from 'fs';
+import json5 from 'json5';
+
 declare var wx: any;
 
-const FileCallbackWebpackPlugin = (): void => {
-  console.log('hello');
-};
+interface FileCallbackPluginOptions {
+  path: string;
+  callback: (content: any) => any;
+}
+class FileCallbackWebpackPlugin {
+  private options: FileCallbackPluginOptions;
+  private processed: boolean;
+
+  constructor(options: FileCallbackPluginOptions) {
+    this.options = options;
+    this.processed = false;
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('FileCallbackPlugin', (compilation, cb) => {
+      const { path, callback } = this.options;
+      const extention = path.split('.').pop();
+      if (this.processed) return cb();
+
+      try {
+        const fileContent = fs.readFileSync(path, 'utf-8');
+        const isJSON = extention === 'json' || extention === 'json5';
+        const content = isJSON ? json5.parse(fileContent) : fileContent;
+        const modifiedContent = isJSON
+          ? JSON.stringify(callback(content), null, 2)
+          : callback(content);
+
+        fs.writeFileSync(path, modifiedContent);
+        this.processed = true;
+        cb();
+      } catch (error) {
+        cb(error);
+      }
+    });
+  }
+}
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
